94b980ec62bffc5404298fc39a8dd8da94356488
Djonathan Goulart <d.goulart@outlook.com.br>
2020-12-22 21:44:53 -0300

    Added header component

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,12 @@ import HomePage from './pages/homepage/homepage';
 
 import './App.css';
 import ShopPage from './pages/shop/shop';
+import Header from './components/header/header';
 
 function App() {
   return (
     <div>
+      <Header/>
       <Switch>
         <Route path='/' component={HomePage} exact/>
         <Route path='/shop' component={ShopPage} />
