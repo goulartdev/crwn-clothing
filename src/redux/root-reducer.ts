352d8181d27d0cd233e70ea5fe7b556ff3341e2d
Djonathan Goulart <d.goulart@outlook.com.br>
2020-12-29 08:32:59 -0300

    - Added redux-persist to persist cart items in local storage

--- src/redux/root-reducer.ts
+++ src/redux/root-reducer.ts
@@ -1,7 +1,15 @@
 import { combineReducers } from "redux";
+import { persistReducer } from "redux-persist";
+import storage from 'redux-persist/lib/storage';
 
 import authReducer from "./auth/auth.reducer";
 import cartReducer from "./cart/cart.reducer";
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['cart']
+}
 
 const rootReducer = combineReducers({
   auth: authReducer,
@@ -10,4 +18,4 @@ const rootReducer = combineReducers({
 
 export type RootState = ReturnType<typeof rootReducer>;
 
-export default rootReducer;
+export default persistReducer(persistConfig, rootReducer);
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,10 +1,13 @@
 import { applyMiddleware, compose, createStore } from "redux";
 import logger from "redux-logger";
+import { persistStore } from "redux-persist";
 
 import rootReducer from "./root-reducer";
 
 const middlewares = compose(applyMiddleware(logger), (window as any).__REDUX_DEVTOOLS_EXTENSION__());
 
 const store = createStore(rootReducer, middlewares);
+
+export const persistor = persistStore(store);
 
 export default store;
